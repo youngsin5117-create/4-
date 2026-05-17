302dcfe5bd4f19ecbeeb7dedd0c3ca700de82b63
youngsin5117-create <youngsin5117@gmail.com>
2026-05-17 19:50:40 +0900

    Delete sw (2).js

--- sw (2).js
+++
@@ -1,4 +0,0 @@
-const CACHE='quiz-v1';
-const ASSETS=['/','/index.html','/manifest.json'];
-self.addEventListener('install',e=>e.waitUntil(caches.open(CACHE).then(c=>c.addAll(ASSETS))));
-self.addEventListener('fetch',e=>e.respondWith(caches.match(e.request).then(r=>r||fetch(e.request))));
