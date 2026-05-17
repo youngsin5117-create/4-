df9426abb55be92c86eb3a45e6e48a2e43c493d7
youngsin5117-create <youngsin5117@gmail.com>
2026-05-17 19:27:36 +0900

    Add files via upload

---
+++ sw (1).js
@@ -0,0 +1,4 @@
+const CACHE='quiz-v1';
+const ASSETS=['/','/index.html','/manifest.json'];
+self.addEventListener('install',e=>e.waitUntil(caches.open(CACHE).then(c=>c.addAll(ASSETS))));
+self.addEventListener('fetch',e=>e.respondWith(caches.match(e.request).then(r=>r||fetch(e.request))));
